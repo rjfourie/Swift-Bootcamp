732570f78cb67c5b2924aee28ecba59ac54dc8ca
Russell <russjfour@gmail.com>
2019-10-11 23:55:36 +0200

    day3

---
+++ Day03/Day03/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Day03
+//
+//  Created by Russell Fourie on 2019/10/11.
+//  Copyright © 2019 Russell Fourie. All rights reserved.
+//
+
+import UIKit
+
+//Acosiating this class to storyboard
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var Image: UIImageView!
+}
---
+++ Day03/Day03/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day03
+//
+//  Created by Russell Fourie on 2019/10/11.
+//  Copyright © 2019 Russell Fourie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
